6ed7b9a9c0c7fd10797359fb7879f345d2ae58e1
shudingbo <shudingbo@qq.com>
2019-01-16 14:59:27 +0800

    Create testlog.js

---
+++ examples/testlog.js
@@ -0,0 +1,21 @@
+const plogger = require('../lib/logger.js');
+
+//var logger = require('@sex-pomelo/sex-pomelo-logger').getLogger('log', __filename, process.pid);
+plogger.configure({
+    appenders: {
+      cheeseLogs: { type: 'file', filename: 'cheese.log' },
+      console: { type: 'console' }
+    },
+    categories: {
+      cheese: { appenders: ['cheeseLogs'], level: 'error' },
+      another: { appenders: ['console'], level: 'trace' },
+      default: { appenders: ['console', 'cheeseLogs'], level: 'trace' }
+    }
+  });
+
+ let logger = plogger.getLogger('log', __filename, process.pid);
+
+process.env.LOGGER_LINE = true;
+logger.info('test1');
+logger.warn('test2');
+logger.error('test3');
